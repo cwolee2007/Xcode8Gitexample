a6f3ce729f4788444eb3e3be0fc16664b0bd884b
LYS <lys@LYSs-MacBook-Pro.local>
2017-04-21 11:54:31 -0700

    Just added a comment

--- Xcode8Gitexample/AppDelegate.swift
+++ Xcode8Gitexample/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+    
+        // additional comment!
     }
+    
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
